bf87473cf933b133b770a294ae6a4a5cd122f37f
leeshanex <leeshanex@outlook.com>
2020-05-11 10:02:15 -0700

    Added list for number 2 and number 3. Working on number 2 logic

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,9 @@ namespace LINQ_Problems
         
         static void Main(string[] args)
         {
+            //-------------------------------------------------------------------------------------------------------------
+            //1)
+
             List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
 
             //where
@@ -22,7 +26,23 @@ namespace LINQ_Problems
             //orderby
 
             //select
-            Console.ReadLine();
+
+            //-------------------------------------------------------------------------------------------------------------
+            //2)
+
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+
+            var removeDuplicates = names.Where(n => n.());
+            foreach (var name in removeDuplicates)
+            {
+                Console.WriteLine(removeDuplicates);
+            }
+
+
+            //-------------------------------------------------------------------------------------------------------------
+            //3)
+             List<string> classGrades = new List<string>() {"80,100,92,89,65","93,81,78,84,69","73,88,83,99,64","98,100,66,74,55"};
+
 
 
         }
